3a8cf412436478905c57f7f797943e0b1f616bdb
Allen <s972024@gmail.com>
2017-03-31 17:00:53 +0800

    basic part

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # RxJavaExample
+
+RxJava Beginner Example
+
+```
+// prework, the <String> can translate to <whatever> you want
+Observer<String> observer = new Observer<String>() {
+@Override
+public void onSubscribe(Disposable d) {
+
+}
+
+@Override
+public void onNext(String s) {
+System.out.println(s);
+}
+
+@Override
+public void onError(Throwable e) {
+
+}
+
+@Override
+public void onComplete() {
+
+}
+};
+
+/** RxJava 1 **/
+/* Observable observable = Observable.create(new Observable.OnSubscribe<String>() {
+@Override
+public void call(Subscriber<? super String> subscriber) {
+subscriber.onNext("Hello");
+subscriber.onNext("Hi");
+subscriber.onNext("Aloha");
+subscriber.onCompleted();
+}
+}); */
+
+/** RxJava 2, o1 is doing the same stuff as o2 and o3 here**/
+Observable<String> o1 = Observable.create(new ObservableOnSubscribe<String>() {
+@Override
+public void subscribe(ObservableEmitter<String> e) throws Exception {
+e.onNext("o1 next1");
+e.onNext("o1 next2");
+e.onComplete();
+}
+});
+
+Observable<String> o2 = Observable.just("o2 next1", "o2 next2"); // which is include onComplete()
+
+String[] words = {"o3 next1", "o3 next2"};
+Observable o3 = Observable.fromArray(words);
+
+// do the stuff!
+o1.subscribe(observer);
+o2.subscribe(observer);
+o3.subscribe(observer);
+'''
--- app/src/main/java/com/allenwang/rxjava/MainActivity.java
+++ app/src/main/java/com/allenwang/rxjava/MainActivity.java
@@ -3,11 +3,139 @@ package com.allenwang.rxjava;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
+import io.reactivex.subscribers.ResourceSubscriber;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // prework, the <String> can translate to <whatever> you want
+        Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                System.out.println(s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+
+        /** RxJava 1 **/
+        /* Observable observable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("Hello");
+                subscriber.onNext("Hi");
+                subscriber.onNext("Aloha");
+                subscriber.onCompleted();
+            }
+        }); */
+
+        /** RxJava 2, o1 is doing the same stuff as o2 and o3 here**/
+        Observable<String> o1 = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> e) throws Exception {
+                e.onNext("o1 next1");
+                e.onNext("o1 next2");
+                e.onComplete();
+            }
+        });
+
+        Observable<String> o2 = Observable.just("o2 next1", "o2 next2"); // which is include onComplete()
+
+        String[] words = {"o3 next1", "o3 next2"};
+        Observable o3 = Observable.fromArray(words);
+
+        // do the stuff!
+        o1.subscribe(observer);
+        o2.subscribe(observer);
+        o3.subscribe(observer);
+
+
+        // general speaking, it is same as observer
+        Subscriber<String> subscriber = new Subscriber<String>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+
+        /** RxJava 1 **/
+        //o1.subscribe(subscriber);
+        /** RxJava 2 **/
+        //way1
+        Flowable.just("subscriber next1").subscribe(subscriber); // onError may not work at RxJava2
+        Flowable.just("subscriber next2").safeSubscribe(subscriber);
+
+        //way2
+        ResourceSubscriber<Integer> resourceSubscriber = new ResourceSubscriber<Integer>() {
+            @Override
+            public void onStart() {
+                request(2);
+            }
+
+            @Override
+            public void onNext(Integer t) {
+                System.out.println(t);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                t.printStackTrace();
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("Done");
+            }
+        };
+        CompositeDisposable composite2 = new CompositeDisposable();
+        composite2.add(Flowable.range(0, 52).subscribeWith(resourceSubscriber));
+
     }
 }
